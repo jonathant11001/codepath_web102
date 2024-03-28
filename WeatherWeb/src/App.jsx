4dac81b77757fcaccd810d6bd843e52b0b619beb
jonathant11001 <jonathant11001@gmail.com>
2024-03-28 17:58:43 -0400

    weather site created

---
+++ WeatherWeb/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const API_KEY = import.meta.env.VITE_APP_API_KEY;
+
+  return (
+    <>https://dataservice.accuweather.com/forecasts/v1/minute?q=40.7128,74.0060&apikey=6GKWQOsATR7met8Ci0IA5Gd3BE8whKwf&language=en-us</>
+  )
+}
+
+export default App
